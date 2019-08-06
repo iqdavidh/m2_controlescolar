f45ee37931f51e92807301f87d5c33110f338db0
Jarsory <arsory92@gmail.com>
2019-08-06 15:43:23 -0500

    login

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -2,37 +2,51 @@
   <div>
     <div class="row">
       <div class="col-md-12">
-        <h1  id="tituloSeccion">Login</h1>
+        <h1 id="tituloSeccion">Login</h1>
       </div>
     </div>
-
+    <form>
+      <div>
+        <label>
+          <b>Usurario</b>
+        </label>
+        <input placeholder="Escriba su correo" />
+      </div>
+      <div>
+        <label>
+          <b>Contraseña</b>
+        </label>
+        <input placeholder="Escriba su contraseña" />
+      </div>
+      <button>Entrar</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember" /> Recordarme
+      </label>
+    </form>
+    <div>
+      <button type="button" class="cancelbtn">Cancelar</button>
+    </div>
+    <span class="psw">
+      Olvidaste tu
+      <a href="#">contraseña?</a>
+    </span>
   </div>
-
 </template>
 
 
 <script>
-
-
-
-  export default {
-    name: 'login',
-    props:{},
-    components: { },
-    data(){
-      return {}
-    },
-    methods:{
-
-    },mounted() {
-
-    }
-
-
-  }
+export default {
+  name: "login",
+  props: {},
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
 </script>
 
 
 <style scoped>
-
 </style>
--- front/src/views/Reportes.vue
+++ front/src/views/Reportes.vue
@@ -4,7 +4,81 @@
             <div class="col-md-12">
                 <h1 id="tituloSeccion">Reportes</h1>
             </div>
+			<div>
+      </div>
+      Seleccionar Grupo
+      <select class="form-control" v-model="idGrupo" @change="onDatosCambiaron">
+        <option :value="g._id" v-for="g in listaGrupos" :key="g._id">{{g.nombre}}</option>
+      </select>
+      <div style="padding-top:20px">
+        Seleccionar fecha
+		<input>
+        <datepicker v-model="fechaDMY" @selected="onDatosCambiaron" format="dd/MM/yyyy"></datepicker>
+      </div>
+	  <div>
+			  Solicitar Tarea
+			  <input>
+		  </div> 
+		
+      <div class="text-center" v-show="!isDatosListos">
+		 
+        <button class="btn btn-primary" @click="mostrarAsistencia">
+          <i class="fa fa-download"></i>
+          Solicitar
+        </button>
+      </div>
+    </div>
+
+    <!-- La tabla de alumnos con la asistencia del grupo en la fecha sel -->
+    <div v-show="isDatosListos">
+      <div class="row">
+        <div class="col-md-12" style="display:flex">
+          <h2 id="tituloSeccion">Asistencia</h2>
+          <div class="panCmd" v-show="!isEdit" style="flex-grow:1; text-align: right">
+            <button type="button" class="btn btn-secondary" @click="onEditar">
+              <i class="fa fa-edit"></i> Modificar
+            </button>
+          </div>
+          <div class="panCmd" v-show="isEdit" style="flex-grow:1; text-align: right">
+            <button type="button" class="btn btn-primary" @click="onGuardar">
+              <i class="fa fa-upload"></i> Actualizar
+            </button>
+          </div>
         </div>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Número de lista</th>
+            <th scope="col">Nombre del alumno</th>
+            <th scope="col">Asistencia</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="alumno in listaAlumnos" :key="alumno.id">
+            <td>{{alumno.id}}</td>
+            <td>{{alumno.apellidos}} {{alumno.nombre}}</td>
+            <td>
+              <div class="text-center" v-show="!isEdit">
+                <span v-show="alumno.fechas[0].valor==1">.</span>
+                <span v-show="alumno.fechas[0].valor==2">R</span>
+                <span v-show="alumno.fechas[0].valor==3">J</span>
+                <span v-show="alumno.fechas[0].valor==0">/</span>
+              </div>
+              <div v-show="isEdit">
+                <select class="form-control" v-model="alumno.fechas[0].valor">
+                  <option value="1">Asistencia</option>
+                  <option value="2">Retardo</option>
+                  <option value="3">Justificación</option>
+                  <option value="0">Falta</option>
+                </select>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    {{idGrupo}}
 
     </div>
 
