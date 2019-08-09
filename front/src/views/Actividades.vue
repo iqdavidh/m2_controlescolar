86ad2c12585a2b652e929681bb45b35c6728c50b
Jarsory <arsory92@gmail.com>
2019-08-09 12:48:32 -0500

    modificar calificaciones

--- front/src/views/Actividades.vue
+++ front/src/views/Actividades.vue
@@ -2,8 +2,7 @@
   <div>
     <div>
       Seleccionar Grupo
-      <select class="form-control" v-model="idGrupo" 
-      @change="onGrupoCambia">
+      <select class="form-control" v-model="idGrupo" @change="onGrupoCambia">
         <option :value="g._id" v-for="g in listaGrupos" :key="g._id">{{g.materia}}/{{g.nombre}}</option>
       </select>
       Seleccionar Actividad
@@ -17,16 +16,62 @@
 
       <div v-show="seRequiereActualizarDatos && idActividad!=='' ">
         <button @click="mostrarCalificaciones">
+          <i></i>
           Solicitar
         </button>
-
       </div>
     </div>
-    <div class="col-md-12">
-      <h1 id="tituloSeccion">Calificaciones</h1>
+    <div>
+      <div class="col-md-12">
+        <h1 id="tituloSeccion">Calificaciones</h1>
+        <div>
+          <button @click="onEditar">
+            <i class="fa fa-upload">Modificar</i>
+          </button>
+        </div>
+        <div>
+          <button @click="onGuardar">
+            <i class="fa fa-upload">Actualizar</i>
+          </button>
+        </div>
+      </div>
     </div>
-    <table>
-    
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Número de lista</th>
+          <th scope="col">Nombre del alumno</th>
+          <th scope="col">Calificaciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="alumno in listaAlumnos" :key="alumno.id">
+          <td>{{alumno.id}}</td>
+          <td>{{alumno.apellidos}} {{alumno.nombre}}</td>
+          <td style="text-align:center">
+            <div class="text-center" v-show="!isEdit">
+              <span v-if="alumno.calificacion===null">NA</span>
+              <span v-if="alumno.calificacion!==null">{{alumno.calificacion}}</span>
+            </div>
+            <div class="text-center" v-show="isEdit">
+              <select class="form-control" v-model="alumno.calificacion">
+                <option >10</option>
+                <option >9</option>
+                <option >8</option>
+                <option >7</option>
+                <option >6</option>
+                <option >5</option>
+                <option >4</option>
+                <option >3</option>
+                <option >2</option>
+                <option >1</option>
+                <option >0</option>
+
+              </select>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </table>
     <br />
     <br />
@@ -56,30 +101,58 @@ export default {
       listaActividades: [],
       listaAlumnos: [],
       seRequiereActualizarDatos: true,
+      isEdit: false,
+      isEnProceso: false
     };
   },
   methods: {
+    onEditar() {
+      this.isEdit = true;
+      this.isEnProceso = false;
+    },
+    async onGuardar() {
+      if(this.isEnProceso){
+        return;
+      }
+      this.isEnProceso=true;
+      let calificacionesUpdate= [];
+      this.listaAlumnos.forEach(alumno =>{
+    
+        calificacionesUpdate.push({
+          id: alumno.id,
+          nombre: alumno.nombre,
+          apellido: alumno.apellido,
+          calificacion: alumno.calificacion
+        })
+        
+      });
+        let respuesta= await dataService.updateCalificacionesActividad(
+          this.idActividad,
+          calificacionesUpdate
+        )
+    },
     onActividadCambia() {
-      this.seRequiereActualizarDatos= false;
+      this.seRequiereActualizarDatos = true;
     },
     async onGrupoCambia() {
-      
       let respuesta = await dataService.getActividadesIndex(this.idGrupo);
       if (!respuesta.success) {
         libToast.alert(respuesta.msg);
         return;
       }
-      this.seRequiereActualizarDatos= false;
       libToast.success("Datos Recibidos");
 
+      this.seRequiereActualizarDatos = true;
       this.listaActividades = respuesta.data.actividades;
-
-
-      console.log(this.listaActividades);
+      this.idActividad = "";
     },
-   async mostrarCalificaciones (){
+    async mostrarCalificaciones() {
       let respuesta = await dataService.getActividad(this.idActividad);
-
+      if (!respuesta.success) {
+        libToast.alert(respuesta.msg);
+        return;
+      }
+      this.listaAlumnos = respuesta.data.alumnos;
     }
   },
   async mounted() {
