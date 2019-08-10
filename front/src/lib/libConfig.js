691aeb29849e57e7480ad37df4965f962a91b3ff
David H <iq.davidh@gmail.com>
2019-08-09 23:56:42 -0500

    Presentación

--- front/src/lib/libConfig.js
+++ front/src/lib/libConfig.js
@@ -1,6 +1,7 @@
 const libConfig = {
 	isDebug: true,
-	urlApi: 'http://localhost:3003/api'
+	/*urlApi: 'http://localhost:3003/api'*/
+	urlApi: 'http://52.37.165.46:3003/api'
 };
 
 export default libConfig;
